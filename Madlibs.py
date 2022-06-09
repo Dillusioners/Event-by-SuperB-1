77b40089c5a11fdf93274240a27bf2a74025a6a6
Itz OP <106766934+ItzOP69@users.noreply.github.com>
2022-06-09 11:22:24 +0530

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,12 @@
+food = input("Enter a food name: ")
+profession = input("Enter a profession: ")
+p2 = "."
+profession = profession + p2
+animal = input("Enter an animal name: ")
+shop = input("Enter a shop name: ")
+place = input("Enter a place name: ")
+place2 = "!"
+place = place + place2
+verb = input("Enter a verb: ")
+name = input("Enter a name: ")
+print("Look, there's an", animal,"in", place, "Said so the", profession, "We picked up the finest", food, "from the nearest shop. We continued to", verb, "in the nearest", shop, "after eating the", food,"before heading out.")
